1c89bb33f62dfbf7f94a3dfc152fcbc7ffb4a2b5
Krishna Kishor Kammaje <krishna.kammaje@yahoo.com>
2023-12-17 10:48:50 +0000

    controller lifecycle event- setting focus

--- webapp/controller/MyController.controller.js
+++ webapp/controller/MyController.controller.js
@@ -7,25 +7,36 @@ sap.ui.define([
         onInit: function () {
             alert("onInit reached");
             var oData = {
-                field1: "",
-                field2: ""
+                field1: "Krishna",
+                field2: "Kammaje"
             };
             var oModel = new JSONModel(oData);
             this.getView().setModel(oModel);
+
+            let inputField1 = this.byId("inputField1");
+            inputField1.setRequired(true);
+        },
+        onBeforeRendering: function(){
+            alert("onBeforeRendering reached");
+            var inputField2 = this.byId("inputField2");
+            if (inputField2) {
+                inputField2.focus();
+            }            
+        },
+        onAfterRendering: function(){
+            alert("onAfterRendering reached");
+            var inputField2 = this.byId("inputField2");
+            if (inputField2) {
+                inputField2.focus();
+            }               
+        },
+        onExit: function(){
+            alert("onExit reached");
         },
         echoInput: function () {
             var input1 = this.byId("inputField1").getValue();
             var input2 = this.byId("inputField2").getValue();
             alert("Values: " + input1 + ", " + input2 );
-        },
-        onBeforeRendering: function(){
-            alert("onBeforeRendering reached");
-        },
-        onAfterRendering: function(){
-            alert("onAfterRendering reached");
-        },
-        onExit: function(){
-            alert("onExit reached");
-        }
+        }        
     });
 });
